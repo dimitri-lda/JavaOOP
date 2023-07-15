fa7529c0062f05ed124b522cead88701a6da0fb3
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-07-15 13:33:03 +0200

    Lesson 5 - Added View, Wizzard and Farmer step

--- src/main/java/gb/Wizzard.java
+++ src/main/java/gb/Wizzard.java
@@ -1,5 +1,7 @@
 package gb;
 
+import java.util.ArrayList;
+
 public abstract class Wizzard extends BaseHero {
     public Wizzard(String name, int hp, int x, int y) {
         super(name, hp, x, y);
@@ -9,10 +11,46 @@ public abstract class Wizzard extends BaseHero {
         super(name, x, y);
     }
 
+    protected int healLimit = 100;
+
     protected abstract int getHealAmount();
 
     public void heal(BaseHero hero) {
+        if (this.healLimit == 0) {
+            return;
+        }
+
+        int healAmount = this.getHealAmount();
+        hero.takeHeal(healAmount);
         System.out.println(super.getName() + " лечит " + hero.getName());
-        hero.takeHeal(this.getHealAmount());
+
+        this.healLimit -= healAmount;
+        if (this.healLimit < 0) {
+            this.healLimit = 0;
+        }
+    }
+
+    @Override
+    public void step(ArrayList<BaseHero> enemies, ArrayList<BaseHero> teamMates) {
+        if (super.isDead()) {
+            return;
+        }
+        BaseHero weakestTeammate = null;
+        for (BaseHero hero : teamMates) {
+            if (hero.isDead()) {
+                continue;
+            }
+            if (weakestTeammate == null || hero.getHp() < weakestTeammate.getHp()) {
+                weakestTeammate = hero;
+            }
+        }
+        if (weakestTeammate != null) {
+            this.heal(weakestTeammate);
+        }
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo();
     }
 }
